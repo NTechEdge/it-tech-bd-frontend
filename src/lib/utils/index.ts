b027e4d9cbfcc3e74642b2965f01f3a1f405a118
eistiakahmed <eistiakahmedmeraj@gmail.com>
2026-04-28 16:15:38 +0600

    feat: add utility library index
    Create centralized export file for all utility functions and classes

---
+++ src/lib/utils/index.ts
@@ -0,0 +1,28 @@
+/**
+ * Utility library index file
+ * Export all utility functions and classes for easy importing
+ */
+
+// Validation
+export * from './validation';
+
+// Formatting
+export * from './format';
+
+// Storage
+export * from './storage';
+
+// Constants
+export * from './constants';
+
+// Helpers
+export * from './helpers';
+
+// Class names
+export * from './classNames';
+
+// Errors
+export * from './errors';
+
+// HTTP Client
+export { httpClient } from './httpClient';
